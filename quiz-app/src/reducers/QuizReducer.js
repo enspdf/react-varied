605b6569ede34920d644b7af400775d80eb2724a
Sebastian Higuita <sebastianhc1995@hotmail.com>
2020-04-11 22:54:31 -0500

    Quiz App

---
+++ quiz-app/src/reducers/QuizReducer.js
@@ -0,0 +1,51 @@
+import {
+  SET_CURRENT_ANSWER,
+  SET_CURRENT_QUESTION,
+  SET_ERROR,
+  SET_SHOW_RESULTS,
+  SET_ANSWERS,
+  RESET_QUIZ,
+} from "./types";
+
+function quizReducer(state, action) {
+  switch (action.type) {
+    case SET_CURRENT_ANSWER:
+      return {
+        ...state,
+        currentAnswer: action.currentAnswer,
+      };
+    case SET_CURRENT_QUESTION:
+      return {
+        ...state,
+        currentQuestion: action.currentQuestion,
+      };
+    case SET_ERROR:
+      return {
+        ...state,
+        error: action.error,
+      };
+    case SET_SHOW_RESULTS:
+      return {
+        ...state,
+        showResults: action.showResults,
+      };
+    case SET_ANSWERS:
+      return {
+        ...state,
+        answers: action.answers,
+      };
+    case RESET_QUIZ:
+      return {
+        ...state,
+        answers: [],
+        currentQuestion: 0,
+        currentAnswer: "",
+        showResults: false,
+        error: "",
+      };
+    default:
+      return state;
+  }
+}
+
+export default quizReducer;
