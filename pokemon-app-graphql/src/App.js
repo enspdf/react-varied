2f75c034a6fff70dfd1badae5f4d4a35003d266b
Sebastian Higuita <sebastianhc1995@hotmail.com>
2020-04-12 01:37:57 -0500

    Pokemon App GraphQL

---
+++ pokemon-app-graphql/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "@apollo/react-hooks";
+import { PokemonsContainer } from "./containers/PokemonsContainer";
+import "./App.css";
+
+function App() {
+  const client = new ApolloClient({
+    uri: "https://graphql-pokemon.now.sh",
+  });
+  return (
+    <ApolloProvider client={client}>
+      <PokemonsContainer />
+    </ApolloProvider>
+  );
+}
+
+export default App;
