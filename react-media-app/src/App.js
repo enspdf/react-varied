9911b941cf88302ee7e13d7741db61f0474210e1
Sebastian Higuita <sebastianhc1995@hotmail.com>
2020-07-08 21:24:50 -0500

    React media setup / navbar added

---
+++ react-media-app/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+import React from 'react';
+
+import Navbar from './assets/layout/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-media-app/src/assets/layout/Navbar.js
@@ -0,0 +1,81 @@
+import React, { useState } from "react";
+import logo from "../../logo.svg";
+
+const Navbar = () => {
+  const [open, setOpen] = useState(false);
+  const openMenu = () => {
+    setOpen(!open);
+  };
+
+  return (
+    <header className="bg-gray-900 sm:flex sm:justify-between sm:items-center sm:px-4 sm:py-3">
+      <div className="flex items-center justify-between px-4 py-3 sm:p-0">
+        <div className="flex items-center">
+          <img className="h-8" src={logo} alt="react logo" />
+          <span className="text-white">Media</span>
+        </div>
+        <div className="sm:hidden">
+          <button
+            type="button"
+            className="block text-gray-500 hover:text-white focus:text-white focus:outline-none"
+            onClick={openMenu}
+          >
+            <svg className="h-6 w-6 fill-current" viewBox="0 0 24 24">
+              {open ? (
+                <path
+                  fillRule="evenodd"
+                  d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"
+                />
+              ) : (
+                <path
+                  fillRule="evenodd"
+                  d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
+                />
+              )}
+            </svg>
+          </button>
+        </div>
+      </div>
+      <nav
+        className={
+          open
+            ? "block px-2 pt-2 pb-4 sm:flex sm:p-0"
+            : "hidden px-2 pt-2 pb-4 sm:flex sm:p-0"
+        }
+      >
+        <a
+          href="#"
+          className="block px-2 py-1 text-white font-semibold rounded hover:bg-gray800"
+        >
+          Login
+        </a>
+        <a
+          href="#"
+          className="mt-1 block px-2 py-1 text-white font-semibold rounded hover:bg-gray-800 sm:mt-0 sm:ml-2"
+        >
+          Sign up
+        </a>
+        <a
+          href="#"
+          className="mt-1 block px-2 py-1 text-white font-semibold rounded hover:bg-gray-800 sm:mt-0 sm:ml-2"
+        >
+          +Add
+        </a>
+        <a
+          href="#"
+          className="mt-1 block px-2 py-1 text-white font-semibold rounded hover:bg-gray-800 sm:mt-0 sm:ml-2"
+        >
+          Welcome, name
+        </a>
+        <a
+          href="#"
+          className="mt-1 block px-2 py-1 text-white font-semibold rounded hover:bg-gray-800 sm:mt-0 sm:ml-2"
+        >
+          Signout
+        </a>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
