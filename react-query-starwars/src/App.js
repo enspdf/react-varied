0a3dc9ae022e6388315b12d38989d6f20202e157
Sebastian Higuita <sebastianhiguita@Sebastians-MacBook-Air.local>
2020-08-06 21:41:09 -0500

    React query starwars sample usage

---
+++ react-query-starwars/src/App.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import Navbar from "./components/Navbar";
+import Planets from "./components/Planets";
+import People from "./components/People";
+import { ReactQueryDevtools } from "react-query-devtools";
+
+function App() {
+  const [page, setPage] = useState("planets");
+
+  return (
+    <>
+      <div className="App">
+        <h1>Star Wars Info</h1>
+        <Navbar setPage={setPage} />
+        <div className="content">
+          {page === "planets" ? <Planets /> : <People />}
+        </div>
+      </div>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </>
+  );
+}
+
+export default App;
