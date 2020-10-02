a0475ba327debedd0e61ddd25d30128292a27b9d
Sebastian Higuita <sebastianhiguita@Sebastians-MacBook-Air.local>
2020-10-01 20:08:21 -0500

    Netflix clone added forms, accordion fixes and features components

--- netflix-clone/src/components/accordion/styles/accordion.js
+++ netflix-clone/src/components/accordion/styles/accordion.js
@@ -16,7 +16,9 @@ export const Inner = styled.div`
 export const Item = styled.div`
   color: white;
   margin-bottom: 10px;
-  max-width: 670px;
+  margin: auto;
+  max-width: 700px;
+  width: 100%;
 
   &:first-of-type {
     margin-top: 3em;
