0cd12144ccdb35d8a460db71e2fc231927fb33f0
Sebastian Higuita <sebastianhiguitaca@gmail.com>
2021-06-03 19:50:52 -0500

    CRA + Express lerna multi package app

---
+++ cra-lerna-express/packages/server-api/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const port = process.env.PORT || 5000
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send("I'm a backend server")
+})
+
+app.listen(port, (err) => {
+    if (err) {
+        console.log(`Error: ${error.msg}`)
+    } else {
+        console.log(`Listening on port ${port}`);
+    }
+})
