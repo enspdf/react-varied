668d99f02763bdf396145c64b6a6500f86bed053
Sebastian Higuita <sebastianhiguitaca@gmail.com>
2021-06-07 00:06:33 -0500

    Simple react app with parcel bundler

---
+++ react-app-parcel-with-parcel/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+export default class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello. This is parcel react app</h1>
+            </div>
+        )
+    }
+}
