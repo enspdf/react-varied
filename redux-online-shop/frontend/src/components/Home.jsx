3bfd29504adc6f711993d3dbe78f88ace45859d9
Sebastian Higuita <sebastianhiguitaca@gmail.com>
2021-08-29 11:47:07 -0500

    redux toolkit show items in the home page

--- redux-online-shop/frontend/src/components/Home.jsx
+++ redux-online-shop/frontend/src/components/Home.jsx
@@ -5,8 +5,24 @@ const Home = () => {
 
   return (
     <div className="home-container">
-      <h2>New Arrivals</h2>
-      <div className="products"></div>
+      {isLoading && <div>Loading...</div>}
+      {error && <p>An error ocurred...</p>}
+      <>
+        New Arrivals
+        <div className="products">
+          {data?.map((product) => (
+            <div key={product.id} className="product">
+              <h3>{product.name}</h3>
+              <img src={product.image} alt={product.name} />
+              <div className="details">
+                <span>{product.desc}</span>
+                <span className="price">${product.price}</span>
+              </div>
+              <button>Add To Cart</button>
+            </div>
+          ))}
+        </div>
+      </>
     </div>
   );
 };
